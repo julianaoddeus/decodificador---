b30e1b520e770def672b337dc79731668d89d8c2
Juliana <64862169+julianaoddeus@users.noreply.github.com>
2022-04-30 23:14:55 -0300

    criado layout mobile
    botões, caixa de mensagem  e input.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let texto = document.querySelector('#texto-cripto');
+let resposta = document.querySelector('.h2');
+let botaoCriptografar = document.querySelector('.button');
+
+function Criptografar() {
+    botaoCriptografar.addEventListener("click", function () {
+
+    });
+
+    for (var i = 0; i < texto.length; i++) {
+        let mudaVogal = "";
+        if (texto[i] == a) {
+            mudaVogal = ai;
+        }
+    }
+    console.log(texto.value);
+
+}
+
+Criptografar();
