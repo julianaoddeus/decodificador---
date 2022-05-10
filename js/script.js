96301e3ddded437c0bca142ab7a8a7455ba021b0
Juliana <64862169+julianaoddeus@users.noreply.github.com>
2022-05-10 19:42:17 -0300

    Atualização texto encriptar

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
 let texto = document.querySelector('#texto-cripto');
-let resposta = document.querySelector('.h2');
+let mensagem = document.querySelector('.titulo--mensagem');
+mensagem.value;
 let botaoCriptografar = document.querySelector('.button');
 
-function Criptografar() {
-    botaoCriptografar.addEventListener("click", function () {
+botaoCriptografar.addEventListener("click", function () {
+    const input = encriptaTexto(texto.value);
+    mensagem.innerHTML = input;
 
-    });
 
-    for (var i = 0; i < texto.length; i++) {
-        let mudaVogal = "";
-        if (texto[i] == a) {
-            mudaVogal = ai;
+});
+
+function encriptaTexto(vogais) {
+    const vogaisMatriz = [
+        ['a', 'ai'],
+        ['e', 'enter'],
+        ['i', 'imes'],
+        ['o', 'ober'],
+        ['u', 'ufat']
+    ];
+    vogais = vogais.toLowerCase();
+
+    for (let i = 0; i < vogaisMatriz.length; i++) {
+        if (vogais.includes(vogaisMatriz[i][0])) {
+            vogais = vogais.replaceAll(vogaisMatriz[i][0], vogaisMatriz[i][1])
         }
     }
-    console.log(texto.value);
 
+    return vogais;
 }
 
-Criptografar();
+
+
+
+
+let botaoCopiar = document.querySelector(".copy");
+
+botaoCopiar.addEventListener("click", function () {
+    let textoCopiado = document.querySelector(".titulo--mensagem");
+    textoCopiado.select();
+    document.execCommand('copy');
+    alert('copiado');
+})
